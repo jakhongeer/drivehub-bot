2d753feadb305b4d783daa8e0dab4934213dc88f
Jahongir Nagmatov <jahongirnagmatov@gmail.com>
2021-06-09 11:37:27 +0500

    add start def

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from telegram.ext import (Updater,
-                          CallbackContext)
+                          CallbackContext,
+                          CommandHandler,
+                          MessageHandler,)
+from telegram import Update
 import logging
 from keys import API_TOKEN
 import logging
@@ -7,10 +10,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.DEBUG)
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_user.id, text="Hi R u read to start?")
+def start_command(update, context):
+    context.bot.send_message(chat_id=update.effective_user.id, text="Hi r u ready to start?")
+
+
+
 
 def main():
     updater=Updater(token=API_TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_command))
+
     updater.start_polling()
     updater.idle()
